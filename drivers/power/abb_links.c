codinakang/platform_kernel_samsung_u8500

Fix LPM charging

drivers/power/abb_links.c
@@ -23,6 +23,10 @@
 extern struct class *power_supply_class;
 extern struct class *sec_class;
 
+#if defined(CONFIG_MACH_JANICE_CHN) || defined(CONFIG_MACH_CODINA_CHN) || defined(CONFIG_MACH_GAVINI_CHN) || defined(CONFIG_MACH_CODINA_EURO) || defined(CONFIG_MACH_CODINA) || defined(CONFIG_MACH_JANICE)
+extern u32 sec_lpm_bootmode;
+#endif
+
 struct charger_extra_sysfs
 {
 	struct device *dev;	
@@ -598,7 +602,11 @@ static int battery_get_property(struct power_supply *psy,
 		break;
 	
 	case POWER_SUPPLY_PROP_LPM_MODE:    /* LPM mode */
+#if defined(CONFIG_MACH_JANICE_CHN) || defined(CONFIG_MACH_CODINA_CHN) || defined(CONFIG_MACH_GAVINI_CHN) || defined(CONFIG_MACH_CODINA_EURO) || defined(CONFIG_MACH_CODINA) || defined(CONFIG_MACH_JANICE)
+		val->intval = sec_lpm_bootmode;
+#else
 		val->intval = charger_extra_sysfs.batt_lp_charging; /* 0 or 1 */
+#endif
 		break;
 
 	case POWER_SUPPLY_PROP_REINIT_CAPACITY:    /* Re-initialize capacity */

onioneq4/-android_kernel_samsung_gavini

drivers/power/abb_links.c
@@ -23,6 +23,10 @@
 extern struct class *power_supply_class;
 extern struct class *sec_class;
 
+#if defined(CONFIG_MACH_JANICE_CHN) || defined(CONFIG_MACH_CODINA_CHN) || defined(CONFIG_MACH_GAVINI) || defined(CONFIG_MACH_CODINA_EURO) || defined(CONFIG_MACH_CODINA) || defined(CONFIG_MACH_JANICE)
+extern u32 sec_lpm_bootmode;
+#endif
+
 struct charger_extra_sysfs
 {
 	struct device *dev;	
@@ -598,7 +602,11 @@ static int battery_get_property(struct power_supply *psy,
 		break;
 	
 	case POWER_SUPPLY_PROP_LPM_MODE:    /* LPM mode */
+#if defined(CONFIG_MACH_JANICE_CHN) || defined(CONFIG_MACH_CODINA_CHN) || defined(CONFIG_MACH_GAVINI) || defined(CONFIG_MACH_CODINA_EURO) || defined(CONFIG_MACH_CODINA) || defined(CONFIG_MACH_JANICE)
+		val->intval = sec_lpm_bootmode;
+#else
 		val->intval = charger_extra_sysfs.batt_lp_charging; /* 0 or 1 */
+#endif
 		break;
 
 	case POWER_SUPPLY_PROP_REINIT_CAPACITY:    /* Re-initialize capacity */